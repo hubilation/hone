4692114dc187dd09fbd9f8e140dc5c078d0a8084
Zack Huber <hubilation@gmail.com>
2026-03-01 16:09:54 -0800

    Initial Commit

---
+++ Practice Timer/Practice_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_TimerApp.swift
+//  Practice Timer
+//
+//  Created by Zack Huber on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Practice_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
